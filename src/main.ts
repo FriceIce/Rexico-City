9ab07dfd1a93eb8a7193dc02ff7c098151128735
Isak Tilahun <tilahun.isak@gmail.com>
2024-12-11 15:59:02 +0100

    Install Tailwind CSS Fixes #3
    Tailwind is now installed and ready to use.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
-
-
+import './index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config';
-import DatePicker from 'primevue/datepicker';
+import PrimeVue from 'primevue/config'
+import DatePicker from 'primevue/datepicker'
 
 const app = createApp(App)
-app.use(PrimeVue);
-app.component('AppDatePicker', DatePicker);
+app.use(PrimeVue)
+app.component('AppDatePicker', DatePicker)
 
 app.use(createPinia())
 app.use(router)
