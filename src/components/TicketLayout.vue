12a0a6ce9dfd2f9e93053d73a4a55578a7265660
Isak Tilahun <tilahun.isak@gmail.com>
2024-12-12 18:01:36 +0100

    Store tickets data in json files and fetch the data in TicketsLayout.vue component or TicketsPage.vue Fixes #9
    - Stored the tickets data in the /public folder at /database/tickets/cards.json
    - Fetching the tickets data using axios and tanstack/vue-query for caching.
    - I moved the images to the public folder to make them accessible via fetching

--- src/components/TicketLayout.vue
+++ src/components/TicketLayout.vue
@@ -1,53 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+import axios from 'axios'
+
+const fetchTickets = async () => {
+  const response = await axios.get('/database/tickets/cards.json')
+  console.log(response.data)
+  return response.data
+}
+
+const { data, isLoading } = useQuery<Array<TicketCard>>({
+  queryKey: ['tickets'],
+  queryFn: () => fetchTickets(),
+})
+</script>
 <template>
   <ul class="card-layout gap-x-6 gap-y-24 flex-wrap">
-    <li class="flex max-w-[360px] bg-[#0e6e61] h-full w-full rounded-lg mx-auto lg:mx-0 shadow-xl">
+    <li
+      v-if="!isLoading && data"
+      v-for="ticket in data"
+      :key="ticket.id"
+      class="flex max-w-[360px] h-full w-full rounded-lg mx-auto lg:mx-0 shadow-xl"
+      :style="`background: ${ticket.background}`"
+    >
       <div class="relative flex flex-col">
         <div class="absolute top-[-64px] left-[50%] translate-x-[-50%] w-[220px]">
           <img
-            src="../assets/images/dino_card.png"
-            alt=""
+            :src="ticket.image.src"
+            :alt="ticket.image.alt"
             class="w-full h-[128px] object-cover object-center rounded-lg shadow-lg"
           />
         </div>
         <section class="flex-1 flex flex-col gap-4 space-y-4 w-full text-center pt-[80px] p-4">
           <div class="flex flex-col gap-3 justify-between">
-            <h1 class="font-bold text-4xl">Green Card</h1>
-            <p class="">
-              Free entry EVERY open day including ALL concerts and dance nights all summer 2025! ⭐️
+            <h1 class="font-bold text-4xl" :style="`color: ${ticket.title.color}`">
+              {{ ticket.title.text }}
+            </h1>
+            <p class="" :style="`color: ${ticket.subtitle.color}`">
+              {{ ticket.subtitle.text }}
             </p>
           </div>
           <div class="flex-1 flex flex-col justify-end gap-3">
-            <p class="text-xl">42 $</p>
+            <p class="text-xl" :style="`color: ${ticket.subtitle.color}`">{{ ticket.price }} $</p>
             <button
-              class="bg-[#e06891] w-full py-[12px] rounded-full lg:hover:scale-105 transition-all"
-            >
-              Buy now
-            </button>
-          </div>
-        </section>
-      </div>
-    </li>
-    <li class="flex max-w-[360px] bg-[#ffebc6] w-full rounded-lg mx-auto lg:mx-0 shadow-xl">
-      <div class="relative flex flex-col">
-        <div class="absolute top-[-64px] left-[50%] translate-x-[-50%] w-[220px]">
-          <img
-            src="../assets/images/gold_card.png"
-            alt=""
-            class="w-full h-[128px] object-cover object-center rounded-lg shadow-lg"
-          />
-        </div>
-        <section class="flex-1 flex flex-col gap-4 space-y-4 w-full text-center pt-[80px] p-4">
-          <div class="flex flex-col gap-3 justify-between">
-            <h1 class="font-bold text-4xl text-[#886611]">Gold Card + Green Card included!</h1>
-            <p class="text-black">
-              Right now, you get the Green Card for free when you purchase a Gold Card! 👀
-            </p>
-          </div>
-          <div class="flex-1 flex flex-col justify-end gap-3">
-            <p class="text-xl text-black">189.9 $</p>
-            <button
-              class="bg-[#2e3038] w-full py-[12px] rounded-full lg:hover:scale-105 transition-all"
+              class="w-full py-[12px] rounded-full lg:hover:scale-105 transition-all"
+              :style="`background: ${ticket.button.bgColor}`"
             >
               Buy now
             </button>
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import DatePicker from 'primevue/datepicker'
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
 
+const queryClient = new QueryClient()
 const app = createApp(App)
+app.use(VueQueryPlugin, {
+  queryClient,
+})
 app.use(PrimeVue)
 app.component('AppDatePicker', DatePicker)
 
