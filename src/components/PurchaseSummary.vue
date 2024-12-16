001c3983add123006c9d6caa7dab369ba01f8340
Isak Tilahun <tilahun.isak@gmail.com>
2024-12-16 16:39:07 +0100

    -I have created a component that shows a summary of the item/items the user intends to add to their shopping cart.
    - The user can access the summary by pressing on "next step" button.
    
    - The "next step" button and left button is disabled by default and will be enabled when the ticket count >= 1.

---
+++ src/components/PurchaseSummary.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+defineProps<{
+  data: Product
+  nextStep: boolean
+  showSummary: boolean
+  price: number
+  ticketCounter: number
+  numberOfTickets: string
+  setShowSummary: () => void
+  setNextStep: () => void
+}>()
+</script>
+<template>
+  <section v-if="nextStep && data" class="flex flex-col items-center gap-6 w-full max-w-[610px]">
+    <div class="bg-[#1b5240] text-white w-full py-8 px-4 rounded">
+      <h3 class="text-3xl">Summary</h3>
+      <div class="space-y-5">
+        <div class="flex items-center gap-4 justify-between">
+          <div class="flex items-center gap-3">
+            <img
+              src="/images/chevron-icon.svg"
+              alt="chevon arrow"
+              @click="setShowSummary()"
+              :class="`size-8 cursor-pointer transition-all ${!showSummary && 'rotate-[180deg]'}`"
+            />
+            <p class="w-max">Tickets</p>
+          </div>
+          <div class="flex gap-4">
+            <p class="">{{ numberOfTickets }}</p>
+            <p class="">$ {{ price }}</p>
+          </div>
+        </div>
+        <!-- This will show the all the tickets selected in the previous section in a list.  -->
+        <div
+          :class="`grid transition-all duration-200 ease-linear
+              ${showSummary ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'}`"
+        >
+          <ul
+            :class="`space-y-4 overflow-hidden border-b transition-all duration-200 ease-in-out ${showSummary && 'py-6'}`"
+          >
+            <li v-for=", in Array(ticketCounter)" class="flex gap-2 justify-between">
+              <p class="ml-11">{{ data.cardType }}</p>
+              <p class="">$ {{ data.price }}</p>
+            </li>
+          </ul>
+        </div>
+        <div class="flex justify-between">
+          <h4 class="font-bold text-xl">Total</h4>
+          <p class="text-xl font-bold">$ {{ price }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex gap-4 w-full">
+      <button
+        class="flex-1 bg-[#e6fbf6] text-[#2a7056] py-[10px] rounded-full border-2 border-[#2a7056]"
+        @click="setNextStep()"
+      >
+        Back
+      </button>
+      <button
+        class="flex-1 bg-[#2a7056] text-white py-[10px] rounded-full border-2 border-[#2a7056]"
+      >
+        Add to cart
+      </button>
+    </div>
+  </section>
+</template>
--- src/views/TicketsNextPage.vue
+++ src/views/TicketsNextPage.vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query'
 import { useRoute } from 'vue-router'
+import { computed, ref, watch } from 'vue'
+import PurchaseSummary from '@/components/PurchaseSummary.vue'
 import axios from 'axios'
-import { ref } from 'vue'
 
 const ticketCounter = ref<number>(0)
+const nextStep = ref<boolean>(false)
+const price = ref<number>(0)
+const showSummary = ref<boolean>(true)
+
+const setShowSummary = () => (showSummary.value = !showSummary.value)
+
+const setNextStep = () => {
+  if (!nextStep) showSummary.value = true // This sets showSummary state to true, ensuring that the "summary-list" is alwasy open when the user initially enters the <PurchaseSummary /> component.
+  nextStep.value = !nextStep.value
+}
+
+const numberOfTickets = computed(() => {
+  const tickets = ticketCounter.value
+  return tickets > 1 ? `${tickets} pcs` : `${tickets} pc`
+})
+
+watch(nextStep, () => {
+  // If the "next step" btn is pressed and the fetched data is truthy then multiply the ticket price with the current ticket counter
+  if (nextStep.value && data.value) {
+    price.value = data.value.price * ticketCounter.value
+  }
+})
 
 // useRoute for accessing route params
 const route = useRoute()
@@ -23,8 +46,8 @@ const { data, isLoading, isError } = useQuery<Product>({
 
 <template>
   <main v-if="!isLoading && data && !isError" class="">
-    <div class="bg-white text-black w-full flex flex-col items-center">
-      <section class="space-y-16 px-4 py-[29px] md:py-20">
+    <div class="bg-white text-black w-full flex flex-col items-center p-6">
+      <section v-if="!nextStep" class="space-y-16 py-[29px] md:py-20">
         <article class="space-y-8 max-w-[610px]">
           <section class="space-y-6">
             <h1 class="font-bold text-3xl md:text-5xl">{{ data.title }}</h1>
@@ -53,7 +76,8 @@ const { data, isLoading, isError } = useQuery<Product>({
             </div>
             <div class="flex items-center gap-4">
               <button
-                :class="`bg-[#2a7056] size-12 rounded-full grid place-items-center lg:cursor-pointer ${ticketCounter === 0 && 'opacity-50'}`"
+                :disabled="ticketCounter === 0"
+                :class="`bg-[#2a7056] size-12 rounded-full grid place-items-center ${ticketCounter === 0 && 'opacity-50 cursor-not-allowed'}`"
                 @click="ticketCounter--"
               >
                 <p class="text-white text-center">-</p>
@@ -68,12 +92,24 @@ const { data, isLoading, isError } = useQuery<Product>({
             </div>
           </div>
           <button
-            :class="`bg-[#006649] text-white w-full py-[12px] rounded-full ${ticketCounter === 0 && 'opacity-50'}`"
+            :disabled="ticketCounter === 0"
+            :class="`bg-[#006649] text-white w-full py-[12px] rounded-full ${ticketCounter === 0 && 'opacity-50 cursor-not-allowed'}`"
+            @click="nextStep = !nextStep"
           >
             Next step
           </button>
         </section>
       </section>
+      <PurchaseSummary
+        :data
+        :next-step
+        :show-summary
+        :ticket-counter
+        :price
+        :number-of-tickets
+        :set-show-summary
+        :set-next-step
+      />
     </div>
   </main>
 </template>
